20d3e08c6a87b5432e4b540b8cd7867db8f136cd
Linwei Zhang <lewiszlw520@gmail.com>
2022-04-29 16:07:43 +0800

    Test protobuf code generation

---
+++ include/raft.h
@@ -0,0 +1,10 @@
+//
+// Created by Linwei Zhang on 2022/4/28.
+//
+
+#ifndef RAFT_RAFT_H
+#define RAFT_RAFT_H
+
+
+
+#endif //RAFT_RAFT_H
---
+++ src/proto_test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Linwei Zhang on 2022/4/29.
+//
+
+#include "raft.pb.h"
+
+int main() {
+    return 0;
+}
